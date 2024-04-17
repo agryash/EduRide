ec0e55f788dc3cb078cf46c61e9e38c26097c02e
Yash Agrawal <agrawal.ya@northeastern.edu>
2024-04-17 18:22:04 -0400

    Added Chat functionality for Pending Requests

--- EduRide/MyTrips/Controller/PendingRequestsViewController.swift
+++ EduRide/MyTrips/Controller/PendingRequestsViewController.swift
@@ -66,8 +66,11 @@ class PendingRequestsViewController: UIViewController {
         
         if let tripId = pendingRequestTrips[tripIndex].id {
             let requestorEmail = pendingRequestTrips[tripIndex].pendingRequests[emailIndex]
+            
             DatabaseManager.shared.acceptPendingRequest(with: requestorEmail, with: tripId)
             showAlert(status: "Success", text: "Accepted \(requestorEmail) request!")
+            
+            
         }
     }
 
@@ -89,7 +92,38 @@ class PendingRequestsViewController: UIViewController {
         let tripIndex = tags[0]
         let emailIndex = tags[1]
         
-        print("Chat button tapped \(tripIndex) \(emailIndex)")
+        if let tripId = pendingRequestTrips[tripIndex].id {
+            let requestorEmail = pendingRequestTrips[tripIndex].pendingRequests[emailIndex]
+            
+            if(requestorEmail == currentUser?.emailAddress){
+                self.showToast(message: "Can't Chat with Yourself", font: .systemFont(ofSize: 12.0))
+            } else{
+                let vc = SingleChatViewController(with: "", id: "")
+                vc.title = requestorEmail
+                vc.otherUserEmail = requestorEmail
+                vc.currSender = (currentUser?.emailAddress)!
+                vc.hidesBottomBarWhenPushed = true
+                navigationController?.pushViewController(vc, animated: true)
+            }
+        }
+    }
+    
+    func showToast(message : String, font: UIFont) {
+        let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 75, y: self.view.frame.size.height-200, width: 150, height: 35))
+        toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+        toastLabel.textColor = UIColor.white
+        toastLabel.font = font
+        toastLabel.textAlignment = .center;
+        toastLabel.text = message
+        toastLabel.alpha = 1.0
+        toastLabel.layer.cornerRadius = 10;
+        toastLabel.clipsToBounds  =  true
+        self.view.addSubview(toastLabel)
+        UIView.animate(withDuration: 4.0, delay: 0.1, options: .curveEaseOut, animations: {
+             toastLabel.alpha = 0.0
+        }, completion: {(isCompleted) in
+            toastLabel.removeFromSuperview()
+        })
     }
     
     func showAlert(status: String, text: String) {
